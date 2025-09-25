3eaee2b501e751f853056dff09ed6939e522fdb2
Zadigo <pendenquejohn@gmail.com>
2025-09-25 23:21:57 +0200

    Refactor project with ignores and src file modificiation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,4 +3,3 @@ export type * from './types'
 export { createAxiosInstance, createVueAxiosManager, VueAxiosManager, vueAxiosManager } from './base'
 export { useAsyncRequest, useAxiosLogin, useRequest } from './composables'
 export { checkDomain, createInternalEndpointName, inProduction } from './utils'
-
