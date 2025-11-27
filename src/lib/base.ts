02bd923d3374afd343e2e568b58a5bfc85d386b5
Zadigo <pendenquejohn@gmail.com>
2025-11-27 18:37:24 +0100

    Defaults endpoints

--- src/lib/base.ts
+++ src/lib/base.ts
@@ -12,6 +12,35 @@ import { inProduction } from './utils'
 export function createVueAxiosManager(options: PluginOptions): Plugin {
   return {
     install(app) {
+      // Set default values for missing endpoint options
+      options.endpoints.forEach((endpointOptions) => {
+        if (!endpointOptions.refreshEndpoint) {
+          endpointOptions.refreshEndpoint = '/v1/token/refresh/'
+        }
+
+        if (!endpointOptions.accessEndpoint) {
+          endpointOptions.accessEndpoint = '/v1/token/'
+        }
+
+        if (!endpointOptions.verifyEndpoint) {
+          endpointOptions.verifyEndpoint = '/v1/token/verify/'
+        }
+
+        if (!endpointOptions.dev) {
+          endpointOptions.dev = '127.0.0.1'
+        }
+
+        if (!endpointOptions.port) {
+          endpointOptions.port = '8000'
+        }
+
+        if (typeof endpointOptions.verifyToken === 'undefined') {
+          endpointOptions.verifyToken = true
+        }
+      })
+
+      // console.log('Initializing Vue Axios Manager with options:', options)
+
       vueAxiosManager.initialize(app, options)
 
       app.mixin({
